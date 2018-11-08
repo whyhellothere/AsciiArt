82787a5d5b3d94293ea1ca8e1c9fd84c544c74d5
Daniel <Daniel@Daniels-MacBook-Pro.local>
2018-11-08 16:37:25 -0600

    driver update

--- AsciiArt.java
+++ AsciiArt.java
@@ -3,35 +3,33 @@ import java.util.Scanner;
 public class AsciiArt {
 
   public static void main(String[] args) {
-
+    
+    // creates important variables
     Scanner scanner = new Scanner(System.in); 
-
     Canvas canvas = startPrompt(scanner);
-
 
     while(true) {
       userPrompt();
       String command = scanner.nextLine();
       String[] commandArray = command.split(" ");
-      if(command=="undo") {
+      if(command.toLowerCase()=="undo") { // if the user calls undo
         canvas.undo();
-      } else if(command=="redo") {
+      } else if(command.toLowerCase()=="redo") { // if the user calls redo
         canvas.redo();
-      } else if(commandArray[0]=="draw") {
-        try {
+      } else if(command.toLowerCase()=="print"){ // if the user calls print
+        System.out.println(canvas.toString());
+      } else if(commandArray[0].toLowerCase()=="draw") { // if the user calls draw
+        try { // surround with a try catch because the user could try to 
           canvas.draw(Integer.parseInt(commandArray[1]), Integer.parseInt(commandArray[2]), 
               commandArray[3].charAt(0));
         } catch(Exception e) {
           System.out.println("invalid command");
           break;
         }
+      } else {
+        System.out.println("invalid command");
       }
     }
-    
-    //    if(same.length!=5) {
-    //      System.out.println("Incorrect command format");
-    //    }
-
   }
 
   private static Canvas startPrompt(Scanner scanner) {
@@ -40,7 +38,7 @@ public class AsciiArt {
     try {
       String[] dimensions = scanner.next().split(" ");
       canvas = new Canvas(Integer.parseInt(dimensions[0]), Integer.parseInt(dimensions[1]));
-    } catch (Exception e) {
+    } catch (Exception e) { // catch IndexOutOfBoundsException, etc
       System.out.println("invalid command");
       startPrompt(scanner);
     }
@@ -48,10 +46,11 @@ public class AsciiArt {
   }
 
   private static void userPrompt() {
-
-    System.out.println("Welcome to the thing");
-    System.out.println("Please enter a character at a location");
-
+    System.out.println("Please avoid unnecessary whitespace. Thank you.");
+    System.out.println("Add a character (row, column, character)");
+    System.out.println("Undo (undo)");
+    System.out.println("Redo (redo)");
+    System.out.println("Print the array (print)");
   }
 
 }
